f21b316ed96fcfbfa5ed6d7e0d6e9fb6bc02f233
Amin Albatayneh <ameenmohammad@live.com>
2022-07-20 21:25:59 -0500

    added post

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
-// create our post model
+// create our Post model
 class Post extends Model { }
 
 // create fields/columns for Post model
