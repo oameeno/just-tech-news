ed38f760cdb44e2428070524363fd71dd02aa898
Amin Albatayneh <ameenmohammad@live.com>
2022-07-23 17:09:56 -0500

    added comments

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,16 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'Asdfu12@', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize('just_tech_news_db', 'root', 'Asdfu12@', {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
